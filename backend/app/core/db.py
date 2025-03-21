dd4748cdb2be07091c0de99149d4f26538ab2d51
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-03-21 01:20:56 +0100

    Feat: OpenAI Azure client config

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,7 @@
 import weaviate
 from groq import Groq
 from app.core.config import settings
-
+from openai import AzureOpenAI
 
 # Weaviate Client
 weaviate_client = None
@@ -9,6 +9,8 @@ weaviate_client = None
 # LLM Client
 groq_client = None
 
+# Azure OpenAI Client
+openai_client = None
 
 # Groq
 def connect_groq():
@@ -42,3 +44,20 @@ def get_weaviate_client():
     connect_weaviate()
     vecdb = weaviate_client
     return vecdb
+
+# OpenAI
+def connect_openai():
+    global openai_client
+    if openai_client is None:
+        openai_client = AzureOpenAI(
+            api_key=settings.OPENAI_API_KEY,
+            api_version=settings.OPENAI_API_VERSION,
+            azure_endpoint=settings.OPENAI_AZURE_ENDPOINT,
+        )
+    return openai_client
+
+
+def get_openai_client():
+    connect_openai()
+    openai = openai_client
+    return openai
